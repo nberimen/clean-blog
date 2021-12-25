0ee9de66dc4fae4fb62bda9a51137e1e9281c275
Necati Berimen <berimen66@gmail.com>
2021-12-25 22:15:07 +0300

    Mongoose CRUD

--- app.js
+++ app.js
@@ -1,24 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
-
+const Post = require('./models/Post');
 const app = express();
+
+//connect DB
+mongoose.connect('mongodb://localhost/clean-blog-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 //MIDDLEWARES
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
-  res.render('index');
+app.get('/', async (req, res) => {
+  const posts = await Post.find({});
+  res.render('index', {
+    posts,
+  });
 });
 app.get('/about', (req, res) => {
   res.render('about');
 });
 app.get('/add_post', (req, res) => {
   res.render('add_post');
+});
+
+app.post('/posts', async (req, res) => {
+  await Post.create(req.body);
+  res.redirect('/');
 });
 
 const port = 3000;
