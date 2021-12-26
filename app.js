2bef6dc7806a59a75252b51a84ff85bcec993a88
Necati Berimen <berimen66@gmail.com>
2021-12-26 16:36:26 +0300

    Update Post

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const ejs = require('ejs');
 const path = require('path');
 const Post = require('./models/Post');
@@ -18,6 +19,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
 
 //ROUTES
 app.get('/', async (req, res) => {
@@ -43,6 +49,21 @@ app.get('/add_post', (req, res) => {
 app.post('/posts', async (req, res) => {
   await Post.create(req.body);
   res.redirect('/');
+});
+
+app.get('/posts/edit/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  res.render('edit', {
+    post,
+  });
+});
+
+app.put('/posts/:id', async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.id });
+  post.title = req.body.title;
+  post.detail = req.body.detail;
+  post.save();
+  res.redirect(`/posts/${req.params.id}`);
 });
 
 const port = 3000;
